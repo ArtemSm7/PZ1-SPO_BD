371fcf2e6abeaf6f453bf3af1fea3cb9d7f869c8
ArtemSm7 <smartfbns@gmail.com>
2026-02-27 12:04:10 +0300

    Update пз3.py

--- пз3.py
+++ пз3.py
@@ -59,13 +59,4 @@ class DB:
         return self
     
     def __exit__(self, *args):
-        self.close()
-        
-
-with DB('users2') as db:
-    # db.create_table({'id': 'INT AUTO_INCREMENT PRIMARY KEY',
-    #            'name': 'VARCHAR(255) NOT NULL',
-    #            'email': 'VARCHAR(255) UNIQUE'})
-    # db.create({'name': 'Ivan', 'email': 'ivan@mail.ru'})
-    db.drop_table()
-    
+        self.close()   
